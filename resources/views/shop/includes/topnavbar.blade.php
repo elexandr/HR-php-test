3f5b44208229d82145b956d4006f563749b938e6
Alexander Syrovatkin <sibadi@gmail.com>
2020-05-30 21:03:20 +0300

    minor changes

--- resources/views/shop/includes/topnavbar.blade.php
+++ resources/views/shop/includes/topnavbar.blade.php
@@ -1,4 +1,3 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="{{ asset('/') }}">Тестовое задание</a>
-    <a target="_blank" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="https://hh.ru/resume/f9ff7082ff07ef95cc0039ed1f653643676971">выполнил Сыроваткин Александр</a>
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="{{ asset('/') }}">Тестовый проект</a>
 </nav>
--- resources/views/shop/layout.blade.php
+++ resources/views/shop/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <title>
-        @yield('title', 'Александр Сыроваткин - Тестовое задание')
+        @yield('title', 'Тестовый проект')
     </title>
 
     @yield('csrf')
